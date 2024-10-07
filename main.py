af9e0d3980e35fdf312baeda70b35745dfb487a6
lisa-vong <lisa.vongg0@gmail.com>
2024-10-07 04:20:00 -0400

    main window

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 import tkinter as tk
 
+class pacemaker(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title ("Pacemaker")
+        self.geometry ("700x600")
+        
+        self.currentFrame = None
+    
+    def switchFrame(self, frame):
+        if self.currentFrame is not None:
+            self.currentFrame.destroy()
+            
+        self.currentFrame = frame(self)
+        self.currentFrame.pack(fill = "both", expand = True)
+                
+
+if __name__ == "__main__":
+    app = pacemaker()
+    app.mainloop()
